413e03ac1ee5fd3f9b949dc7b96f498a723fa307
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-04-04 07:16:56 -0400

    adjusted readme

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Dataflow for ScenePro
 To edit RDF mapping:
 1. Run `./run_ontorefine.sh` (if needed `chmod +x run_ontorefine.sh`)
 2. Open localhost:7333 to use Open Refine workbench
-3. Open the existing project 
+3. Open the existing project
 4. Click "Edit RDF Mapping"
 5. When done making changes save RDF Mapping
 6. Clean up the history (tab undo/redo) to keep only the steps needed.
--- run_ontorefine.sh
+++ run_ontorefine.sh
@@ -10,16 +10,15 @@ while ! curl --output /dev/null --silent --head --fail http://localhost:7333; do
 done
 echo "Server started!"
 
-# Send a command to a running container
-echo "Running OntoRefine CLI to load config.json..."
+# Send a command to the running container
+echo "Running OntoRefine CLI using config.json..."
 docker exec onto_refine /opt/ontorefine/dist/bin/ontorefine-cli transform ../data/entities.json \
   -u http://localhost:7333  \
   --no-clean \
   --configurations ../data/config.json  \
   -f json >> scenepro-entities.ttl
-echo "Open Project to edit the RDF Mapping."
-echo "Afterwards export project configurations and commit back to config.json."
 
 # Open the default browser
 open http://localhost:7333
 
+echo "Open Project to edit the RDF Mapping."
